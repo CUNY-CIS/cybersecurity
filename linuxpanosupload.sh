4912cb126f38a4d5809814ae6e7d0c347a9351e0
Sung.Lee04 <Sung.Lee@cuny.edu>
2026-04-21 11:23:58 -0400

    Added author and license GPLv3 information for script

--- linuxpanosupload.sh
+++ linuxpanosupload.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# Copyright (C) 2026 
+#
+# Author: Sung Lee
+# Organization: City University of New York (CUNY)
+# Maintainer: Sung Lee <sung.lee@cuny.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# --------------------------------------------------------------------
+# Palo Alto Networks PAN-OS Certificate Import & Commit Script
+# --------------------------------------------------------------------
+#
 ###############################################################################
 # PURPOSE
 #   Import a PEM keypair (certificate + private key) into a Palo Alto Networks
